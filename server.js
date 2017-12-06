3d4c8b7bd08cc8ac2cd520a890185e84106e1ee2
Jérémy Mathon <jeremy.mathon@epitech.eu>
2017-12-06 17:43:29 +0100

    server for heroku

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
+var mongoose = require('mongoose');
 
+mongoose.connect('mongodb://heroku_p5j44gpx:5ge1blr3uhcifhchu4bc49pi7k@ds113906.mlab.com:13906/heroku_p5j44gpx', { useMongoClient: true });
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
